92391acab7a3015b6933d44babd3ba9eed03dba4
Joey Jacobs <joeynjacobs1231@gmail.com>
2026-04-13 03:37:08 +0000

    Update Apps Script URL to final v5 (auto-import from Overview)

--- api/sync.js
+++ api/sync.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(200).end();
 
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyd8OqmeoTJRsaBVIyyvBeBfpJcjtS-QIjFZtKyWqwEhNmNpe_NAFs6k0lHmx6SOThj/exec";
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyzyrsU7cIEH0LBUz-HW76MVzINEdaVAA5YvNN1zhkaH4QEakyMuiDHFzXYcjVAx_4AlQ/exec";
 
   try {
     if (req.method === "GET") {
